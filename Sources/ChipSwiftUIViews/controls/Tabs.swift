dc528369adb4698e526de0e8c520f6752b98f393
Benny Chew <minghuibenny@gmail.com>
2021-04-06 14:16:03 +0200

    added tabs

--- Sources/ChipSwiftUIViews/controls/Tabs.swift
+++ Sources/ChipSwiftUIViews/controls/Tabs.swift
@@ -1,8 +1,77 @@
-//
-//  File.swift
-//  
-//
-//  Created by Benny Chew on 03/04/2021.
-//
+import SwiftUI
 
-import Foundation
+///
+/// Tabs
+///
+public struct Tabs: View {
+  @EnvironmentObject private var theme: AppTheme
+
+  public typealias OnTap = (Int) -> Void
+
+  @Binding private var activeIndex: Int
+  private let items: [TabItem]
+  private let spacing: CGFloat
+  private let roundness: Roundness
+  private let baseColor: Color
+  private let onTap: OnTap
+
+  public init(
+    activeIndex: Binding<Int>,
+    items: [TabItem],
+    baseColor: Color,
+    roundness: Roundness = .md,
+    spacing: CGFloat = 6,
+    onTap: @escaping OnTap
+  ) {
+    _activeIndex = activeIndex
+    self.items = items
+    self.baseColor = baseColor
+    self.roundness = roundness
+    self.spacing = spacing
+    self.onTap = onTap
+  }
+
+  public var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      Column(spacing: spacing) {
+        ForEach(items.indices) { (index: Int) in
+          let item = items[index]
+          if index == activeIndex {
+            ClassicButton(
+              item.title,
+              frameColor: baseColor,
+              textColor: theme.colors.altText1,
+              roundness: roundness
+            ) {
+              print("Already current active index.")
+            }
+            .disabled(true)
+          } else {
+            ClassicButton(
+              item.title,
+              frameColor: baseColor.opacity(0.1),
+              textColor: baseColor,
+              roundness: roundness
+            ) {
+              activeIndex = index
+              onTap(index)
+            }
+          }
+        }
+      }
+      .padding(10)
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+}
+
+public extension Tabs {
+  struct TabItem: Identifiable {
+    public let id: UUID = UUID()
+    public let title: String
+
+    public init(title: String) {
+      self.title = title
+    }
+  }
+}
--- Sources/ChipSwiftUIViews/visualisation/CircleProgressView.swift
+++ Sources/ChipSwiftUIViews/visualisation/CircleProgressView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 ///
-/// CircularProgressView
+/// BindableCircularProgressView
 ///
-public struct CircularProgressView: View {
+public struct BindableCircularProgressView: View {
   @Binding private var progress: Float
   private let frameSize: CGFloat
   private let lineWidth: CGFloat
@@ -18,6 +18,51 @@ public struct CircularProgressView: View {
     progressColor: Color = .blue
   ) {
     _progress = progress
+    self.frameSize = frameSize
+    self.lineWidth = lineWidth
+    self.baseColor = baseColor
+    self.progressColor = progressColor
+  }
+
+  public var body: some View {
+    let padding = lineWidth / 2
+    return ZStack {
+      Circle()
+        .stroke(lineWidth: lineWidth)
+        .opacity(0.2)
+        .foregroundColor(baseColor)
+        .padding(padding)
+
+      Circle()
+        .trim(from: 0, to: CGFloat(min(progress, 1)))
+        .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+        .rotationEffect(Angle(degrees: 270))
+        .foregroundColor(progressColor)
+        .padding(padding)
+        .animation(.linear)
+    }
+    .frameSize(frameSize)
+  }
+}
+
+///
+/// CircularProgressView
+///
+public struct CircularProgressView: View {
+  private let progress: Float
+  private let frameSize: CGFloat
+  private let lineWidth: CGFloat
+  private let baseColor: Color
+  private let progressColor: Color
+
+  public init(
+    progress: Float,
+    frameSize: CGFloat = 40,
+    lineWidth: CGFloat = 4,
+    baseColor: Color = .gray,
+    progressColor: Color = .blue
+  ) {
+    self.progress = progress
     self.frameSize = frameSize
     self.lineWidth = lineWidth
     self.baseColor = baseColor
